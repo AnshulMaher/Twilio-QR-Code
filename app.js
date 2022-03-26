56b8c53ed7420a8e847700ffb6b002d9d91688b9
anshul maher <anshul.maher@azent.com>
2022-03-26 15:00:44 +0530

    changed file structure

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
-const { generateQR } = require("./services/qr-code");
-const { sendMessage } = require("./services/sms");
-const { getMessageText } = require("./utils/get-message");
-
+const { publicFolder } = require("./constants");
+const RootRouter = require("./routes/root");
+const SendCodeRouter = require("./routes/send-code");
+const UnownRouter = require("./routes/unknown");
 const app = express();
-const port = process.env.PORT || 8000;
 
 /* implement cors */
 app.use(cors());
@@ -21,38 +19,17 @@ app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 /* serving static files */
-app.use(express.static(path.join(__dirname, process.env.PUBLIC_FOLDER)));
+app.use(express.static(path.join(__dirname, publicFolder)));
 
-app.post("/send-qr-code", async (req, res, next) => {
-    const message = getMessageText(req.body);
+/* initialize routes */
+app.use(RootRouter);
+app.use(SendCodeRouter);
+app.use(UnownRouter);
 
-    try {
-        const codeResponse = await generateQR(message);
-        if (codeResponse.status === 0) return res.status(500).json(codeResponse);
-
-        /* 
-        Uncomment this line when proper set-up is done
-        const qrCodePublicPath = `${process.env.API_URL}/${process.env.PUBLIC_FOLDER}/${codeResponse.data.fileName}`;
-        */
-
-        // temporarily sending test image
-        const qrCodePublicPath = "https://www.park2jetdenver.com/assets/images/home-banner/1.jpg";
-        const messageResponse = await sendMessage(req.body.phone_number, message, {
-            mediaUrl: [qrCodePublicPath],
-        });
-        if (messageResponse.status === 0) return res.status(500).json(messageResponse);
-
-        res.status(200).json({ status: 1, message: "Message Send Successfully" });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ status: 0, message: "Internal Server Error" });
-    }
-});
-app.get("*", (req, res) => {
-    res.status(200).json({ message: "Hello World" });
+/* global error handler */
+app.use((error, req, res, next) => {
+    if (error.isOperational) return res.status(error.statusCode).json({ status: 0, message: error.message });
+    res.status(500).json({ status: 0, message: error.message || "Internal Server Error" });
 });
 
-const server = app.listen(port, (error) => {
-    if (error) throw error;
-    console.log(`listening on port ${port}...`);
-});
+module.exports = app;
--- services/qr-code.js
+++ services/qr-code.js
@@ -1,13 +1,17 @@
 const path = require("path");
 const QRCode = require("qrcode");
 
+const { apiUrl, publicFolder, qrCodesFolder } = require("../constants");
+
 exports.generateQR = async (text) => {
     try {
         const fileName = `qr-code-${Date.now()}.png`;
-        const filePath = path.join(__dirname, "..", process.env.QR_CODE_FOLDER, fileName);
+        const filePath = path.join(__dirname, "..", publicFolder, qrCodesFolder, fileName);
+        const fileUrl = `${apiUrl}/${qrCodesFolder}/${fileName}`;
 
         await QRCode.toFile(filePath, text);
-        return { status: 1, data: { filePath, fileName } };
+
+        return { status: 1, data: { fileUrl, fileName } };
     } catch (error) {
         return { status: 0, message: error.message };
     }
